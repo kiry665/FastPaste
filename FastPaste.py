dad0677acf4f225763cac67fa7f41499a4a99148
kiry665 <51780313+kiry665@users.noreply.github.com>
2023-11-21 16:05:44 +0500

    alfa version 1.0.0

--- FastPaste.py
+++ FastPaste.py
@@ -4,7 +4,7 @@ import sqlite3
 import os
 import pyperclip
 from pynput.keyboard import Key, Controller
-import time
+import threading
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -12,14 +12,31 @@ class Ui_MainWindow(object):
         MainWindow.setStatusTip("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        #QtWidgets.QTreeWidget
         self.treeWidget = Ui_MainWindow.create_tree_from_database(self, Ui_MainWindow.get_abspath('Local.db'), "Tree")
-
-        self.treeWidget.setGeometry(QtCore.QRect(0, 0, 400, 500))
+        self.treeWidget.setGeometry(QtCore.QRect(0, 0, 400, 471))
+        self.treeWidget.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.treeWidget.setObjectName("treeWidget")
-
         self.treeWidget.header().setVisible(False)
-        MainWindow.setCentralWidget(self.centralwidget)
 
+
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(10, 475, 51, 21))
+        self.checkBox.setText("Закрыть")
+        self.checkBox.adjustSize()
+        self.checkBox.setObjectName("checkBox")
+        self.checkBox.setChecked(True)
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(370, 473, 25, 25))
+        self.pushButton.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(Ui_MainWindow.get_abspath("Images/cross.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(19, 19))
+        self.pushButton.setObjectName("pushButton")
+
+        MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -28,8 +45,13 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "FastPaste"))
         __sortingEnabled = self.treeWidget.isSortingEnabled()
         self.treeWidget.setSortingEnabled(False)
-
         self.treeWidget.setSortingEnabled(__sortingEnabled)
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:
+            if self.windowState() & Qt.WindowMinimized:
+                print("Окно свёрнуто")
+                event.accept()
 
     def create_tree_from_database(self, database_file, table_name):
         conn = sqlite3.connect(database_file)
@@ -85,6 +107,18 @@ class Ui_MainWindow(object):
 
 class MyTreeWidget(QtWidgets.QTreeWidget):
     #TODO Сделать нумерацию ветвей дерева для ориентации цифрами
+    def __init__(self, parent=None):
+        super(MyTreeWidget, self).__init__(parent)
+        self.itemSelectionChanged.connect(self.handle_item_selection)
+        self.itemCollapsed.connect(self.itemCollapse)
+        self.tooltip = QtWidgets.QLabel(self)
+        self.tooltip.setWindowFlags(QtCore.Qt.ToolTip)
+        self.timer = QTimer()
+        self.keyboard = Controller()
+
+    #Строгий фокус
+    def focusOutEvent(self, event):
+        self.setFocus()
     def keyPressEvent(self, event):
         key = event.key()
         current_item = self.currentItem()
@@ -112,7 +146,7 @@ class MyTreeWidget(QtWidgets.QTreeWidget):
             Qt.Key_O,
             Qt.Key_P,
         ]
-
+        # Обработка нажатия цифр и символов для выбора элемента в дереве
         if key in keys:
             digit = keys.index(key) + 1
             if(parrent_item is not None and parrent_item.childCount() >= digit):
@@ -123,21 +157,27 @@ class MyTreeWidget(QtWidgets.QTreeWidget):
         # Обработка нажатия Enter для раскрытия элемента или вставки текста
         if key == QtCore.Qt.Key_Return or key == QtCore.Qt.Key_Enter:
             if (current_item is not None and current_item.childCount() > 0):
-                self.expandItem(current_item)
-                self.setCurrentItem(current_item.child(0))
+                if current_item.isExpanded():
+                    self.setCurrentItem(current_item.child(0))
+                else:
+                    self.expandItem(current_item)
+
             if (current_item is not None and current_item.childCount() == 0):
                 data = current_item.data(0, QtCore.Qt.UserRole)
                 pyperclip.copy(str(data))
+
                 MainWindow.hide()
 
-                keyboard = Controller()
+                #keyboard = Controller()
 
-                with keyboard.pressed(Key.ctrl):
-                    keyboard.press('v')
-                    keyboard.release('v')
-                #time.sleep(1)
+                with self.keyboard.pressed(Key.ctrl):
+                    self.keyboard.press('v')
+                    self.keyboard.release('v')
 
-                MainWindow.show()
+                if(MainWindow.checkBox.isChecked()):
+                    QtCore.QTimer.singleShot(100, lambda: app.quit())
+                else:
+                    MainWindow.show()
 
         # Обработка нажатия Backspace для возврата на уровень выше
         if key == QtCore.Qt.Key_Backspace:
@@ -151,20 +191,31 @@ class MyTreeWidget(QtWidgets.QTreeWidget):
                 pass
                 #TODO клавиша ВВЕРХ
         if key == QtCore.Qt.Key_Down:
-            print("Up")
+            if current_item.isExpanded():
+                print("EXP")
+        self.handle_item_selection()
             #TODO клавиша вниз
 
-def handle_item_selection():
-    selected_items = ui.treeWidget.selectedItems()
-    if selected_items:
-        selected_item = selected_items[0]
-        data = selected_item.data(0, QtCore.Qt.UserRole)  # Получаем данные из пользовательской части элемента
-        print("Selected Data:", data)
-        position = ui.treeWidget.visualItemRect(selected_item).topRight()
-        window_position = MainWindow.geometry().topLeft()
-        tooltip_position = window_position + position
-        QtWidgets.QToolTip.showText(tooltip_position, str(data))
+    def handle_item_selection(self):
+        selected_items = self.selectedItems()
+        if selected_items:
+            selected_item = selected_items[0]
+            data = selected_item.data(0, QtCore.Qt.UserRole)  # Получаем данные из пользовательской части элемента
+            position = ui.treeWidget.visualItemRect(selected_item).topRight()
+            window_position = MainWindow.geometry().topLeft()
+            tooltip_position = window_position + position
 
+            self.tooltip.hide()
+            if(str(data) != ""):
+                self.tooltip.setText(str(data))
+                self.tooltip.move(tooltip_position)
+                self.tooltip.show()
+                self.timer.setSingleShot(True)
+                self.timer.timeout.connect(self.tooltip.hide)
+                self.timer.start(5000)
+
+    def itemCollapse(self, item):
+        self.setCurrentItem(item)
 
 if __name__ == "__main__":
     import sys
@@ -172,7 +223,5 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    ui.treeWidget.itemSelectionChanged.connect(handle_item_selection)
-
     MainWindow.show()
     sys.exit(app.exec_())
